99a632fc85cbffd3ad30900659a2887f703e4b7c
edwintantawi <edwintantawi.workspace@gmail.com>
2022-04-23 22:28:11 +0700

    create models, provider, and utils

---
+++ lib/models/color_scheme_data_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorSchemeData {
+  String name;
+  Color color;
+
+  ColorSchemeData({required this.name, required this.color});
+}
---
+++ lib/providers/ui_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:devnote/models/color_scheme_data_model.dart';
+
+class UIProvider extends ChangeNotifier {
+  List<ColorSchemeData> colorSchemaList = [
+    ColorSchemeData(
+      name: 'TR_COLOR_SCHEME.BLUE',
+      color: const Color(0xFF6297FF),
+    ),
+    ColorSchemeData(
+      name: 'TR_COLOR_SCHEME.GREEN',
+      color: Colors.green,
+    ),
+    ColorSchemeData(
+      name: 'TR_COLOR_SCHEME.PURPLE',
+      color: Colors.purple[400]!,
+    ),
+    ColorSchemeData(
+      name: 'TR_COLOR_SCHEME.PINK',
+      color: Colors.pink,
+    ),
+  ];
+
+  int _colorSchemeIndex = 0;
+
+  int get colorSchemeIndex => _colorSchemeIndex;
+
+  void setColorSchemeIndex(newValue) {
+    _colorSchemeIndex = newValue;
+    notifyListeners();
+  }
+}
